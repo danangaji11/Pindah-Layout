c9da945426212c8091b918b3673e969836b15db2
justdanang <danangaji11@users.noreply.github.com>
2022-10-03 11:16:22 +0700

    PindahLayout

--- app/src/main/java/com/if3b/pindah_layout/MainActivity.java
+++ app/src/main/java/com/if3b/pindah_layout/MainActivity.java
@@ -2,13 +2,29 @@ package com.if3b.pindah_layout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+        Button btnPindah;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("Layout A");
+
+        btnPindah = findViewById(R.id.btnPindah);
+
+        btnPindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindah = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(pindah);
+            }
+        });
     }
 }
